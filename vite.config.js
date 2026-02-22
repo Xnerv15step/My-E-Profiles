e2b727f1eec85efe1248b60cc295b322bcfa3758
Xnerv15stepa <bbaabb0424@gmail.com>
2026-02-23 02:18:37 +0800

    系統設定

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My-E-Profiles/', // 務必確保斜線與大小寫與 Repo 名稱一致
 })
